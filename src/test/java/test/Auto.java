c31bd5e95197f9de8e9cbb4a3614a6a8b339e184
sebasz-a <jpabuena@unal.edu.co>
2023-08-20 22:39:07 -0500

    Prueba1

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -4,7 +4,7 @@ public class Auto {
 	
 	public String modelo;
 	public int precio;
-	public Asiento[] asiento;
+	public Asiento[] asientos;
 	public String marca;
 	public Motor motor;
 	public int registro;
@@ -12,12 +12,31 @@ public class Auto {
 	
 	public int cantidadAsientos() {
 		
+		int cAsientos = 0;
 		
+		for (int i = 0; i < asientos.length; i++) {
+			
+			if (asientos[i] instanceof Asiento) {
+				cAsientos++;
+			}
+		}
+		
+		return cAsientos;
 	}
 	
 	public String verificarIntegridad() {
 		
-		
+		if (this.registro == this.motor.registro){
+			
+			for (int i = 0; i < asientos.length; i++) {
+				
+				if (asientos[i].registro != this.registro){
+					return "Las piezas no son originales";
+				}else{
+					return "Auto original";
+				}
+			}
+		}
 	}
 
 }
